3d74da76540830c3867b30d9af3a88356ade2871
= <=>
2023-02-03 15:56:42 +0000

    added skeletton for cashRegister()

---
+++ cash_register.js
@@ -0,0 +1,29 @@
+function cashRegister(price, cash, cid) {
+  let change = 0;
+  let closed = false;
+  let message = { status, change };
+  if (closed) {
+    return message;
+  }
+  if (cash < price) {
+    return message;
+  }
+  if (cid < change) {
+    return message;
+  } else {
+    return message;
+  }
+}
+
+// Example function call
+cashRegister(19.5, 20, [
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100],
+]);
