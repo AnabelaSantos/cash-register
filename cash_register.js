8d370bc2596c82a5bc2f6b6327e89f9fc2fc6099
anabelaSantos <anabelamsantos@gmail.com>
2023-02-04 20:21:28 +0000

    WIP still missing case 3 and 4

--- cash_register.js
+++ cash_register.js
@@ -3,45 +3,62 @@ moneyValue = {
   NICKEL: 0.05,
   DIME: 0.1,
   QUARTER: 0.25,
-  ONE: 1,
-  FIVE: 5,
-  TEN: 10,
-  TWENTY: 20,
-  "ONE HUNDRED": 100,
+  ONE: 1.0,
+  FIVE: 5.0,
+  TEN: 10.0,
+  TWENTY: 20.0,
+  "ONE HUNDRED": 100.0,
 };
 
 function cashRegister(price, cash, cid) {
-  let result = { status: "", change: [] };
+  let change = [];
+  let cidAfterChangeGiven = cid;
+  let result = { status: "", change: change };
 
   //case 1 cash<price
   if (price > cash) {
-    //return {status: "INCORRECT_PAYMENT", change: []}
-    return { status: "INCORRECT_PAYMENT", change: [] };
+    return { status: "INCORRECT_PAYMENT", change: change };
   }
 
   //case 2a cid < change
-  let changeValue = price - cash;
-  let totalValueInCid =
-    cid.PENNY +
-    cid.NICKEL +
-    cid.DIME +
-    cid.QUARTER +
-    cid.ONE +
-    cid.FIVE +
-    cid.TEN +
-    cid.TWENTY +
-    cid["ONE HUNDRED"];
+  let changeValue = cash - price;
+
+  let totalValueInCid = 0;
+  for (let i = 0; i < cid.length; i++) {
+    totalValueInCid += cid[i][1];
+  }
+  console.log(totalValueInCid);
+  console.log(changeValue);
+  console.log(cid[0]);
 
   if (totalValueInCid < changeValue) {
-    // return {status: "INSUFFICIENT_FUNDS", change: []}
-    return { status: "INSUFFICIENT_FUNDS", change: [] };
+    return { status: "INSUFFICIENT_FUNDS", change: change };
   }
 
   //case 2b can't return the exact change
 
-  // return {status: "INSUFFICIENT_FUNDS", change: []}
+  // iterate trough cid to chack if we have the correct currency type
+  let checkChangeValue = changeValue;
+  for (let i = cid.length + 1; i <= 0; i++) {
+    let typeCurrency = cid[i][0]; // eg TEN
+    let typeCurrencyAmount = cid[i][1]; // eg 40
+    let typeCurrencyValue = moneyValue[typeCurrency]; // eg 10
+    while (checkChangeValue >= typeCurrencyAmount) {
+      checkChangeValue -= typeCurrencyValue;
+      typeCurrencyAmount -= typeCurrencyValue;
+      totalValueInCid -= typeCurrencyValue;
+    }
+  }
+  if (checkChangeValue !== 0) {
+    return { status: "INSUFFICIENT_FUNDS", change: change };
+  }
+
   //case 3 cid===change due
-  // Return {status: "CLOSED", change: [...]} with cash-in-drawer as the value for the key change
+  else if (totalValueInCid === 0) {
+    return { status: "CLOSED", change: change };
+  }
+
+  // Return  with cash-in-drawer as the value for the key change
   //   if it is equal to the change due. Include each currency unit in the drawer, even if its value is zero.
   //   (i.e. DO display ["NICKEL", 0])
   // case 4a change due < less total cid && exact change can be made
@@ -51,16 +68,46 @@ function cashRegister(price, cash, cid) {
 }
 
 console.log(
-  cashRegister(19.5, 18, [
-    ["PENNY", 1.01],
-    ["NICKEL", 2.05],
-    ["DIME", 3.1],
-    ["QUARTER", 4.25],
-    ["ONE", 90],
-    ["FIVE", 55],
-    ["TEN", 20],
-    ["TWENTY", 60],
-    ["ONE HUNDRED", 100],
+  cashRegister(19.5, 20, [
+    ["PENNY", 0.5],
+    ["NICKEL", 0],
+    ["DIME", 0],
+    ["QUARTER", 0],
+    ["ONE", 0],
+    ["FIVE", 0],
+    ["TEN", 0],
+    ["TWENTY", 0],
+    ["ONE HUNDRED", 0],
   ])
 );
+/* 
+{
+  status: "CLOSED",
+  change: [
+    ["PENNY", 0.5],
+    ["NICKEL", 0],
+    ["DIME", 0],
+    ["QUARTER", 0],
+    ["ONE", 0],
+    ["FIVE", 0],
+    ["TEN", 0],
+    ["TWENTY", 0],
+    ["ONE HUNDRED", 0]
+  ]
+}
+*/
+
 // => {status: "INCORRECT_PAYMENT", change: []}
+
+// EXAMPLE INVOCATION, so you can `console.log` the outputs
+cashRegister(19.5, 20, [
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100],
+]);
