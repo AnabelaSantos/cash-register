1e1584d5cef35cd333b37a58af09c0e57918f033
anabelaSantos <anabelamsantos@gmail.com>
2023-02-05 21:51:25 +0000

    cash register working

--- cash_register.js
+++ cash_register.js
@@ -9,15 +9,17 @@ moneyValue = {
   TWENTY: 20.0,
   "ONE HUNDRED": 100.0,
 };
-
 function cashRegister(price, cash, cid) {
   let change = [];
   let cidAfterChangeGiven = cid;
+  let cidReversed = cid.reverse();
   let result = { status: "", change: change };
 
   //case 1 cash<price
   if (price > cash) {
-    return { status: "INCORRECT_PAYMENT", change: change };
+    result.status = "INCORRECT_PAYMENT";
+
+    return result;
   }
 
   //case 2a cid < change
@@ -27,46 +29,55 @@ function cashRegister(price, cash, cid) {
   for (let i = 0; i < cid.length; i++) {
     totalValueInCid += cid[i][1];
   }
-  console.log(totalValueInCid);
-  console.log(changeValue);
-  console.log(cid[0]);
 
   if (totalValueInCid < changeValue) {
-    return { status: "INSUFFICIENT_FUNDS", change: change };
+    result.status = "INSUFFICIENT_FUNDS";
+
+    return result;
   }
 
+  // case 3 cid===change due
+  if (totalValueInCid === changeValue) {
+    result.status = "CLOSED";
+    result.change = cid.reverse();
+    return result;
+  }
   //case 2b can't return the exact change
 
-  // iterate trough cid to chack if we have the correct currency type
+  // iterate trough cid to check if we have the correct currency type
   let checkChangeValue = changeValue;
-  for (let i = cid.length + 1; i <= 0; i++) {
-    let typeCurrency = cid[i][0]; // eg TEN
-    let typeCurrencyAmount = cid[i][1]; // eg 40
+  for (let i = 0; i < cidReversed.length; i++) {
+    let typeCurrency = cidReversed[i][0]; // eg TEN
+    let typeCurrencyAmount = cidReversed[i][1]; // eg 40
     let typeCurrencyValue = moneyValue[typeCurrency]; // eg 10
-    while (checkChangeValue >= typeCurrencyAmount) {
-      checkChangeValue -= typeCurrencyValue;
-      typeCurrencyAmount -= typeCurrencyValue;
-      totalValueInCid -= typeCurrencyValue;
+    if (checkChangeValue >= typeCurrencyValue && typeCurrencyAmount > 0) {
+      let newEntryChange = [typeCurrency, 0.0];
+      while (checkChangeValue >= typeCurrencyValue && typeCurrencyAmount > 0) {
+        checkChangeValue -= typeCurrencyValue;
+        typeCurrencyAmount -= typeCurrencyValue;
+        totalValueInCid -= typeCurrencyValue;
+        newEntryChange[1] += typeCurrencyValue;
+        checkChangeValue = Number(checkChangeValue.toFixed(2));
+      }
+      change.push(newEntryChange);
     }
+    cidAfterChangeGiven[i][1] -= Math.round(typeCurrencyValue.toFixed(2));
   }
-  if (checkChangeValue !== 0) {
-    return { status: "INSUFFICIENT_FUNDS", change: change };
+  checkChangeValue = checkChangeValue.toFixed(2);
+  totalValueInCid = Math.round(totalValueInCid.toFixed(2));
+
+  if (checkChangeValue !== "0.00") {
+    result.status = "INSUFFICIENT_FUNDS";
+    //  result.change = [];
+    return result;
   }
 
-  //case 3 cid===change due
-  else if (totalValueInCid === 0) {
-    return { status: "CLOSED", change: change };
-  }
-
-  // Return  with cash-in-drawer as the value for the key change
-  //   if it is equal to the change due. Include each currency unit in the drawer, even if its value is zero.
-  //   (i.e. DO display ["NICKEL", 0])
-  // case 4a change due < less total cid && exact change can be made
-  //   return {status: "OPEN", change: [...]}, with the change due in coins and bills, as the value of the change key.
-  //   Only include the value of a currency unit if its value is not zero. (i.e. do NOT display ["NICKEL", 0])
-  // case 4 b Additional example, only returning some coins/bills (no zero values)
+  //case 4 Otherwise, return {status: "OPEN", change: [...]}, with the change due in coins and bills, as the value of the change key.
+  //Only include the value of a currency unit if its value is not zero. (i.e. do NOT display ["NICKEL", 0])
+  result.status = "OPEN";
+  result.change = change.reverse();
+  return result;
 }
-
 console.log(
   cashRegister(19.5, 20, [
     ["PENNY", 0.5],
@@ -96,18 +107,3 @@ console.log(
   ]
 }
 */
-
-// => {status: "INCORRECT_PAYMENT", change: []}
-
-// EXAMPLE INVOCATION, so you can `console.log` the outputs
-cashRegister(19.5, 20, [
-  ["PENNY", 1.01],
-  ["NICKEL", 2.05],
-  ["DIME", 3.1],
-  ["QUARTER", 4.25],
-  ["ONE", 90],
-  ["FIVE", 55],
-  ["TEN", 20],
-  ["TWENTY", 60],
-  ["ONE HUNDRED", 100],
-]);
