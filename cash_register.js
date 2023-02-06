f7869bf553540c3db5f7a5bc3d3e7ab1ce618f37
= <=>
2023-02-06 02:07:55 +0000

    optimisation

--- cash_register.js
+++ cash_register.js
@@ -1,5 +1,4 @@
 function cashRegister(price, cash, cid) {
-  let changeDueStart = cash - price;
   let changeDue = cash - price;
   let cidTotal = 0;
   for (let i in cid) {
@@ -18,23 +17,34 @@ function cashRegister(price, cash, cid) {
     return message;
   }
   //Case 2a
-  if (cidTotal < changeDueStart) {
+  if (cidTotal < changeDue) {
     message.status = "INSUFFICIENT_FUNDS";
+    return message;
+  }
+  //case 3
+  if (changeDue === cidTotal) {
+    message.status = "CLOSED";
+    message.change = cid;
     return message;
   } else {
     for (let i in currencies) {
       //loop through array curencies and use modulo to see if it is possible to use that currency for giving back the change.
-      // Multiply by 100 to get rid of the decimals and avoid problems.
+      // Multiply by 100 to get rid of the decimals and avoid problems with value < 1.
       let changeC = changeDue * 100;
       let value = (changeC - (changeC % currencies[i])) / 100;
-      //compare the change and the state of the currency in the till to know if there is enough to give back change.
+      //value of 0 means it is not possible to give back change using this currency and cidR[i][1] = 0 means there is no change in that currency so we can skip that iterration.
+      if (value === 0 || cidR[i][1] === 0) {
+        continue;
+      }
+      //Take the maximum amount possible in that currency to give back the change.
       if (cidR[i][1] >= value) {
         change.push([cidR[i][0], value]);
         //had to take care of the decimal as it was preventing the algorithm to log the change properly.
         changeDue = changeDue - value;
         changeDue = parseFloat(changeDue).toFixed(2);
       }
-      //added this case to make the algorithm more efficient and use the bigger notes when possible before checking the smaller one.
+      //added this case to make the algorithm more efficient and use the bigger notes when possible before checking the smaller one,
+      //take the maximum available and deducted that from the changeDue.
       if (cidR[i][1] < value) {
         change.push([cidR[i][0], cidR[i][1]]);
         //decimal issues.
@@ -42,35 +52,22 @@ function cashRegister(price, cash, cid) {
         changeDue = parseFloat(changeDue).toFixed(2);
       }
     }
-    //case 3
-    if (changeDueStart === cidTotal) {
-      message.status = "CLOSED";
-      message.change = change.reverse();
-      return message;
-    }
     //case 2b
     if (changeDue > 0) {
       message.status = "INSUFFICIENT_FUNDS";
       return message;
     }
     //case 4a and b
-    //loop through change and remove zero values
     else {
-      let change2 = [];
-      for (let i in change) {
-        if (change[i][1] != 0) {
-          change2.push([change[i][0], change[i][1]]);
-        }
-      }
       message.status = "OPEN";
-      message.change = change2.reverse();
+      message.change = change.reverse();
       return message;
     }
   }
 }
 //Example function call
 console.log(
-  cashRegister(3.26, 100, [
+  cashRegister(3.26, 60, [
     ["PENNY", 1.01],
     ["NICKEL", 2.05],
     ["DIME", 3.1],
